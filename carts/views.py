fed9df2b366823b66d86044adf8357e6f8a5fe1e
Tzu-Fan Tang <proxvision@gmail.com>
2021-10-01 19:11:50 +0200

    Variation model

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -12,6 +12,24 @@ def _get_cart_id(request):
     return cart_id
 
 def add_to_cart(request, product_id):
+    # Get the product
+    product = Product.objects.get(id=product_id)
+    # Initialise empty variation list
+    product_variation = []
+    # Check for product variations
+    if request.method == 'POST':
+       for key in request.POST:
+           value = request.POST[key]
+           try:
+               variation = Variation.object.get(
+                   product=product,
+                   variation_category__iexact=key, 
+                   variation_value__iexact=value
+                )
+               product_variation.append(variation)
+           except:
+               pass
+        
     try:
         # Get the cart using the car_id present in the session
         cart = Cart.objects.get(
@@ -25,35 +43,65 @@ def add_to_cart(request, product_id):
     # Save present session's cart
     cart.save()
     
-    # Get the product
-    product = Product.objects.get(id=product_id)
     
-    # Add the product to the cart
-    try:
-        # Check if product is already in cart
-        cart_item = CartItem.objects.get(
-            product=product, 
+    # Check if specific product already in cart
+    is_product_in_cart = CartItem.objects.filter(
+        product=product,
+        cart=cart
+    ).exists()
+    # If so, grab the item(s)
+    if is_product_in_cart:
+        cart_item = CartItem.object.filter(
+            product=product,
             cart=cart
         )
-        # If so, increment by 1
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
+        ## Check if current variation (in product_variations list) exists in models (ie. it's already been added to cart) 
+        existing_var_list = []
+        item_id_list = []
+        for item in cart_item:
+            existing_variation = item.variations.all() 
+            existing_var_list.append(list(existing_variation)) # Cast QuerySet as List
+            item_id_list.append(item.id)
+        
+        if product_variation in existing_var_list:
+            ## Increment this item's (of same variation) quantity
+            # Grab corresponding index for item.id when the lists were appended
+            item_id_index = existing_var_list.index(product_variation)
+            # Grab cart item using product and id 
+            item = CartItem.objects.get(
+                product=product, 
+                id=item_id_list[item_id_index]
+            )
+            item.quantity += 1
+            item.save()
+            
+        else: 
+            ## Initialise one CartItem of this product in this cart session
+            item = CartItem.objects.create(
+                product=product,
+                quantity=1,
+                cart=cart,
+            )
+            if product_variation is not None:
+                item.variations.clear()
+                item.variations.add(*product_variation) # unpack as list
+            item.save()
+    else:
         # If product not already in cart, create a CartItem object, with the ForeignKeys
         # and starting at the quantity 1
-        cart_item = CartItem.objects.create(
+        item = CartItem.objects.create(
             product=product,
             cart=cart,
             quantity=1
         )
-        cart_item.save()
-    ## Test if cart_item has been saved
-    # return HttpResponse(cart_item.quantity)
-    # exit()
+        if product_variation is not None:
+            item.varations.clear()
+            item.variations.add(*product_variation) # unpack as list
+        item.save()
     ## Redirect to URLname 'cart' once item has been added
     return redirect('cart')
 
-def decrement_from_cart(request, product_id):
+def decrement_from_cart(request, product_id, cart_item_id):
     # Get current cart
     cart = Cart.objects.get(
         cart_id=_get_cart_id(request)
@@ -62,20 +110,25 @@ def decrement_from_cart(request, product_id):
     product = get_object_or_404(
         Product, id=product_id
     )
-    # Get the cart_item object that is to be removed using the ForeignKeys
-    cart_item = CartItem.objects.get(
-        product=product, 
-        cart=cart
-    )
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    
+    try:
+        # Get the cart_item object that is to be removed using the ForeignKeys
+        cart_item = CartItem.objects.get(
+            product=product, 
+            cart=cart,
+            id=cart_item_id
+        )
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
 
     return redirect('cart')
 
-def remove_from_cart(request, product_id):
+def remove_from_cart(request, product_id, cart_item_id):
     # Get current cart
     cart = Cart.objects.get(
         cart_id=_get_cart_id(request)
@@ -87,7 +140,8 @@ def remove_from_cart(request, product_id):
     # Get the cart_item object that is to be removed using the ForeignKeys
     cart_item = CartItem.objects.get(
         product=product, 
-        cart=cart
+        cart=cart, 
+        id=cart_item_id
     )
     cart_item.delete()
     return redirect('cart')
--- store/models.py
+++ store/models.py
@@ -26,3 +26,32 @@ class Product(models.Model):
     
     def __str__(self):
         return self.product_name
+
+
+class VariationManager(model.Model):
+    """
+    This model inherits from Variation and we define methods to filter 'variation_set', not just '.all' (everything in the set)
+    """
+    def colours(self):
+        return super(Variation, self).filter(variation_category='colour', is_active=True)
+    
+    def sizes(self):
+        return super(Variation, self).filter(variation_category='size', is_active=True)
+
+
+
+class Variation(models.Model):
+    variation_category_choice = (
+        ('colour', 'colour'), 
+        ('size', 'size'),
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+    
+    objects = VariationManager()
+    
+    def __str__(self):
+        return self.variation_value
